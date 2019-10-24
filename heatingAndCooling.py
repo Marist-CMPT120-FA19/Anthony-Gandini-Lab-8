b66bad8a926613bd86a4498cda62fea116cbb87d
Tones55 <54862709+Tones55@users.noreply.github.com>
2019-10-24 17:25:42 -0400

    initial commit

---
+++ heatingAndCooling.py
@@ -0,0 +1,18 @@
+#Anthony Gandini
+
+def main():
+    temperatures = input("Enter the average daily temperatures seperated by spaces: ")
+    coolingDegreeDays = 0
+    heatingDegreeDays = 0
+    
+    for i in temperatures:
+        if int(i) > 80 or int(i) < 60:
+            if int(i) > 80:
+                coolingDegreeDays += int(i) - 80
+            else:
+                heatingDegreeDays += 60 - int(i) 
+    
+    print("There are" , coolingDegreeDays , "cooling degree days.")
+    print("There are" , heatingDegreeDays , "heating degree days")
+    
+main()
